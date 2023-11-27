5d9be23d17991ef4a1641d89b0a7f5734d3fd0f3
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-27 23:30:41 +0900

    데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  안녕하세요~~~~~~~
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>{{rooms[0]}} 원룸</h4>
+    <p>{{price[0]}} 만원</p>
+  </div>
+  <div>
+    <h4>{{rooms[1]}} 원룸</h4>
+    <p>{{price[1]}} 만원</p>
+  </div>
+  <div>
+    <h4>{{rooms[2]}} 원룸</h4>
+    <p>{{price[2]}} 만원</p>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      price : [65, 60, 75],
+      rooms : ['전대리원룸', "둔전원룸", "유방동원룸"],
+      
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
