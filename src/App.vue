3c47519637632b295f2797a7673a55bdfbb3bddc
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-28 23:04:23 +0900

    버튼 이벤트 리스너

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
   <div v-for="(product, i) in products" :key="i">
     <h4>{{product.roomName}} 원룸</h4>
     <p>{{product.price}} 만원</p>
+    <button @click="declaration(i)">허위매물신고</button> <span>신고수 : {{ product.declarations }}</span>
   </div>
 
 </template>
@@ -19,11 +20,16 @@ export default {
     return {
       mainMenus: ['Home', 'Shop', 'About'],
       products: [
-        {roomName: '전대리원룸', price: 65},
-        {roomName: '둔전원룸', price: 60},
-        {roomName: '유방동원룸', price: 75}
-      ]
-      
+        {roomName: '전대리원룸', price: 65, declarations: 0},
+        {roomName: '둔전원룸', price: 60, declarations: 0},
+        {roomName: '유방동원룸', price: 75, declarations: 0}
+      ],
+    }
+  },
+  methods: {
+    declaration: function(id) {
+      this.products[id].declarations += 1;
+      // return
     }
   },
   components: {
